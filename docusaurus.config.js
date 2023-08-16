0f884b9db31fdc7461f85bb09791cf5d91919cdd
Christian Gaege <christian.gaege@averbis.com>
2023-08-16 13:06:39 +0200

    Add health discovery branding to landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Unleash the Potential of Health Data',
-  tagline: 'Extract relevant information and gain insights with Healthcare NLP',
+  title: 'Documentation',
+  tagline: 'Explore our guides and examples to integrate Health Discovery',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 import styles from './index.module.css';
 
@@ -12,13 +13,14 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+      <img src={useBaseUrl('/img/health-discovery-logo.png')} alt="Health Discovery Logo"  height="100" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Quick Start - 15 min &#128640;
+            to="/docs/category/quickstart">
+            Quick Start Tutorial &#x2794;
           </Link>
         </div>
       </div>
@@ -30,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title="Health Discovery Docs"
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
