d85a5949230a022566b6e9dc2c2ddbee1fc3432e
Christian Gaege <christian.gaege@averbis.com>
2023-08-18 15:25:11 +0200

    Re-enabled ghPages build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://help-dev.averbis.com',
+  url: 'https://cgaege.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/health-discovery/',
+  baseUrl: '/docusaurus/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'cgaege', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
